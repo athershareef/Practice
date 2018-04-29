2f6d15a9257ed07fdcd6d6ed9db466779c1ba663
athershareefs <mohammed.shareef@copart.com>
2018-04-29 18:49:08 -0500

    isPermutationPalindrome

--- src/com/string/Main.java
+++ src/com/string/Main.java
@@ -1,16 +1,42 @@
 package com.string;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(permutation("ab").toString());
+        System.out.println(isPermutationPalindrome("never odd or even "));
 
     }
 
-    private static HashSet<String> permutation(String input) {
+    public static boolean isPermutationPalindrome(String str){
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(int i=0; i<str.length(); i++){
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) +1);
+        }
+
+        boolean isOdd = false;
+
+        for(Integer each: map.values()){
+            if(each % 2 != 0 ){
+                if(isOdd){
+                    return false;
+                }
+                isOdd = true;
+            }
+        }
+
+        return true;
+    }
+
+
+    private static HashSet<String> permutationRECURSION(String input) {
         return permutation("", input, new HashSet<String>());
     }
 
@@ -25,4 +51,39 @@ public class Main {
 
         return result;
     }
+
+    public static boolean containsDuplicateCharN2(String str){
+        for(int i = 0; i< str.length() ; i++){
+            char each = str.charAt(i);
+
+            for(int j= i+1; j< str.length(); j++ ){
+
+                if(each == str.charAt(j)){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+
+    }
+
+    public static boolean containsDuplicateCharNLOGN(String str){
+
+        char [] charArr = str.toCharArray();
+
+        Arrays.sort(charArr);
+        System.out.println(charArr);
+
+        for(int i = 0; i < charArr.length-1 ; i++){
+            if(charArr[i] == charArr[i+1]){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+
 }
