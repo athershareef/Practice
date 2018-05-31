376d54cd1096dc49407bd66f72116d5585ed924e
athershareef <athershareefvce@gmail.com>
2018-05-31 14:08:47 -0500

    Array

--- src/com/array/ArrayMain.java
+++ src/com/array/ArrayMain.java
@@ -1,9 +1,34 @@
 package com.array;
 
+import java.util.Arrays;
+
 public class ArrayMain {
 
     public static void main(String[] args) {
-        find3SubSeqN(new int[]  {4, 3, 2, 1} );
+
+        find3SubSeqN(new int[]  {1, 3, 2, 6} );
+        swapMaxNumber(new int[]  {7, 3, 4, 6},2);
+    }
+
+    public static void swapMaxNumber(int[] array, int count){
+        for(int i =0; i < count ; i++){
+            int max = findMax(array, i);
+            int temp = array[i];
+            array[i] = array[max];
+            array[max] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int findMax(int[] array, int index){
+        int max = -1;
+        for(int i = index; i< array.length ; i++){
+            if(max  < array[i]){
+                max = i;
+            }
+        }
+        return array[max] > array[index] ? max : index;
     }
 
    public static void find3SubSeqN(int[] array){
