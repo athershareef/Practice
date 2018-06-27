bffaff053b7eaed4ab590349b4602c0e94b229d0
athershareef <athershareefvce@gmail.com>
2018-06-27 16:48:15 -0500

    String

--- src/com/medium/MediumMain.java
+++ src/com/medium/MediumMain.java
@@ -7,11 +7,71 @@ public class MediumMain {
     public static void main(String[] args) {
 //        System.out.println(findMinSumDP(new int[][] { {2,3,4}, {5,8,15}, {3,4,10} , {5,10,6}}));
 //        System.out.println(findMinSumDP(new int[][] { {2,5,3,5}, {3,8,4,10}, {4,15,10,6}}));
-          System.out.println(mountain(new int[]{2,1,4,7,3,2,1,5}));
-        System.out.println(mountain(new int[]{2,2,2}));
-        System.out.println(largeValue(new Vector<String>()));
+//          System.out.println(mountain(new int[]{2,1,4,7,3,2,1,5}));
+//        System.out.println(mountain(new int[]{2,2,2}));
+//        System.out.println(largeValue(new Vector<String>()));
+
+//            System.out.println(longestCommonPrefix(new String[]{"whatcanIdo", "whatcanIdo", "bhatcanIdo"}));
+//        System.out.println(removeDuplicatesSorted(new int[] {0,0,1,1,1,2,2,3,3,4}));
+        System.out.println(removeNumberFromArray(new int[] {4,5}, 5));
+    }
+
+    public static int removeNumberFromArray(int[] array, int val){
+        int j= 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] != val){
+               array[j] = array [i];
+               j++;
+            }
+        }
+
+        return j;
+    }
+
+
+    public static int removeDuplicatesSorted(int[] array){
+        int length =  array.length;
+        for(int i = 0, j = 0; i < array.length-1 ; i++){
+            if(array[i] == array[i+1]){
+                length--;
+            } else {
+                j++;
+                array[j] = array[i + 1];
+            }
+        }
+
+        return length;
+    }
+
+    public static String longestCommonPrefix(String[] input){
+
+        if(input.length == 0){
+            return "";
+        }
+
+        String result = input[0];
+
+        for(String each: input){
+            result = findIntersection(result, each);
+        }
+
+        return result.length() > 0 ? result : "No common prefix exists!";
 
     }
+
+    private static String findIntersection(String s1, String s2){
+        for(int i =0 ; i < Math.min(s1.length(), s2.length()) ; i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                return s1.substring(0,i);
+            }
+        }
+
+        return s1.length() >= s2.length() ? s2: s1;
+
+
+    }
+
+
 
     public static String largeValue(Vector<String> array) {
 
